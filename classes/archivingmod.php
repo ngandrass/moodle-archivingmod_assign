6001534eee368e0428f03155f9a04fbd2728a246
Niels Gandraß <niels@gandrass.de>
2025-03-22 16:41:53 +0100

    Create plugin skeleton

---
+++ classes/archivingmod.php
@@ -0,0 +1,60 @@
+<?php
+
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Assignment activity archiving driver
+ *
+ * @package     archivingmod_assign
+ * @copyright   2025 Niels Gandraß <niels@gandrass.de>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace archivingmod_assign;
+
+use local_archiving\driver\archivingmod_base;
+
+// @codingStandardsIgnoreFile
+defined('MOODLE_INTERNAL') || die(); // @codeCoverageIgnore
+
+
+/**
+ * Assignment activity archiving driver
+ */
+class archivingmod extends archivingmod_base {
+
+    /**
+     * @inheritDoc archivingmod_base::get_name()
+     */
+    public static function get_name(): string {
+        return get_string('pluginname', 'archivingmod_assign');
+    }
+
+    /**
+     * @inheritDoc archivingmod_base::get_supported_activities()
+     */
+    public static function get_supported_activities(): array {
+        return ['assign'];
+    }
+
+    /**
+     * @inheritDoc archivingmod_base::get_task_settings_form()
+     */
+    public function get_task_settings_form(string $handler, \cm_info $cminfo): \local_archiving\form\job_create_form {
+        return new form\job_create_form($handler, $cminfo);
+    }
+
+}
---
+++ settings.php
@@ -0,0 +1,43 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Plugin administration pages are defined here
+ *
+ * @package     archivingmod_assign
+ * @copyright   2025 Niels Gandraß <niels@gandrass.de>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die(); // @codeCoverageIgnore
+
+
+global $DB;
+
+if ($hassiteconfig) {
+    $settings = new admin_settingpage('archivingmod_assign_settings', new lang_string('pluginname', 'archivingmod_assign'));
+
+    // phpcs:ignore Generic.CodeAnalysis.EmptyStatement.DetectedIf
+    if ($ADMIN->fulltree) {
+        // Descriptive text.
+        $settings->add(new admin_setting_heading('archivingmod_assign/header_docs',
+            null,
+            'TODO: Write something nice here ;)'
+        ));
+    }
+
+    // Settingpage is added to tree automatically. No need to add it manually here.
+}
